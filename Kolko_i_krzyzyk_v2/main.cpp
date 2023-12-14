b9d688da4c7888c607333f06310a751a790d77bb
s32193 <s32193@pjwstk.edu.pl>
2023-12-14 20:55:05 +0100

    maly update main.cpp -> uporzadkowanie -> if breakujacy while loop

--- Kolko_i_krzyzyk_v2/main.cpp
+++ Kolko_i_krzyzyk_v2/main.cpp
@@ -9,14 +9,10 @@ int main()
 {
 	char playing_field[9] = { 49,50,51,52,53,54,55,56,57};
 	int turn = 1;
-	bool continue_loop = true;
 	display_field(playing_field);
-	
-	
 
-	while (continue_loop) {
+	while (true) {
 		
-	
 		int player_index;
 		int player_symbol;
 		
@@ -30,35 +26,39 @@ int main()
 			player_index = 1;
 			player_symbol = 88;
 		}
+		
 		int game_ended = is_game_resolved(turn,playing_field,player_symbol);
 		
 		switch (game_ended)
 		{
 		case 0:
 			cout << endl << "Gracz O wygral." << endl;
-			continue_loop = false;
 			break;
 			
 		case 1:
 			cout << endl << "Gracz X wygral." << endl;
-			continue_loop = false;
 			break;
 		case 2:
 			cout << endl << "AD MORTEM INIMICUS." << endl;
-			continue_loop = false;
 			break;
 		}
 
+		if (game_ended == 0 || game_ended == 1 || game_ended == 2) 
+		{
+			break;
+		}
+		
 		choose_field(player_index, playing_field, player_symbol);
+		display_field(playing_field);
+		
+		turn++;
+	}
+}
+
 			
 		
 		
-		display_field(playing_field);
 
-		turn++;
-	}
 	
 
 
-
-}
